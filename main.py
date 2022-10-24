2819604a4ff736db387965056e3ece7e1d6a93ea
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-10-24 18:38:29 -0400

    initial commit
    day 1

---
+++ main.py
@@ -0,0 +1,7 @@
+import project_1
+#Run Day 1 Project code
+def main():
+    project_1.band_name_generator()
+
+if __name__ == '__main__':
+    main()
---
+++ project_1.py
@@ -0,0 +1,5 @@
+def band_name_generator():
+    print('Hello! Welcome to the Band Name Generator!')
+    city_name = input('What city did you grow up in?\n')
+    pet_name = input('What is a name for a pet?\n')
+    print(f'Your band name could be {city_name} {pet_name}!')
