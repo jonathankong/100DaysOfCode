0e6da0ea266331cb902c959dd05686bd8f0b38c5
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-10-24 20:39:11 -0400

    Day 2 project

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import project_1
+#import project_1
+import project_2
 #Run Day 1 Project code
 def main():
-    project_1.band_name_generator()
+    #project_1.band_name_generator()
+    project_2.tip_calculator()
 
 if __name__ == '__main__':
     main()
---
+++ project_2.py
@@ -0,0 +1,9 @@
+def tip_calculator():
+    print('Welcome to the tip calculator.')
+    total = float(input('What was the total bill? $'))
+    tip = int(input('What percentage tip would you like to give? 10, 12, or 15? '))
+    num_of_people = int(input('How many people to split the bill? '))
+    split_payment = round(total * (1 + tip / 100) / num_of_people, 2)
+    print(f'Each person should pay: ${split_payment}')
+
+    
